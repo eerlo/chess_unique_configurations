bfcc4948624bf3618cf213b1cd38234ff235aff0
Eduardo Erlo <eduardo.erlo@gmail.com>
2016-08-08 19:59:01 -0300

    Tests cotinue and print results on screen.

--- objects.py
+++ objects.py
@@ -11,11 +11,13 @@ class Piece(object):
         self.posx = posx
         self.posy = posy
 
+    def piece_representation(self):
+            return self.representation
+
 
 class King(Piece):
 
-    def __unicode__(self):
-        return u'K'
+    representation = u'K'
 
     def get_possible_moves(self):
         moves = [(self.posx+1, self.posy),
@@ -31,8 +33,7 @@ class King(Piece):
 
 class Rook(Piece):
 
-    def __unicode__(self):
-        return u'K'
+    representation = u'R'
 
     def get_possible_moves(self):
         return [(self.posx+i, self.posy) \
@@ -56,7 +57,7 @@ class Board(object):
         """
         self.board_width = board_width
         self.board_height = board_height
-        self._board_data = [{u'has_piece': False, u'disabled': False} for x in range(board_width*board_height)]
+        self._board_data = [{u'piece': None, u'disabled': False} for x in range(board_width*board_height)]
 
     def __getitem__(self, item):
         return self._board_data[item * self.board_width: (item * self.board_width) + self.board_width]
@@ -79,23 +80,53 @@ class Board(object):
 ee = Board(3, 3)
 result = []
 def resolve(board, pieces):
+    #import ipdb;ipdb.set_trace()
     for piece_n, piece in enumerate(pieces, 1):
         for pos_na, posa in board.enumerate():
+            piece_ok = True
             board2 = board.copy()
             pos_n = pos_na
             pos = board2[pos_n[0]][pos_n[1]]
-            if not pos[u'disabled'] and not pos[u'has_piece']:
-                pos[u'has_piece'] = True
+            if not pos[u'disabled'] and not pos[u'piece']:
+                pos[u'piece'] = piece
                 for move in piece(pos_n[0], pos_n[1], board2).get_possible_moves():
-                    if not board2[move[0]][move[1]][u'disabled'] and not not board2[move[0]][move[1]][u'has_piece']:
+                    if not board2[move[0]][move[1]][u'piece']:
                         board2[move[0]][move[1]]['disabled'] = True
                     else:
+                        piece_ok = False
                         break
-                if not pieces[piece_n:]:
+                if not piece_ok:
+                    continue
+
+                if not pieces[piece_n:] and piece_n == 1:
                     result.append(board2)
                 else:
                     resolve(board2.copy(), pieces[piece_n:])
             else:
-                break
+                continue
 
 
+aa = Board(3,3)
+resolve(aa, [King, King, Rook])
+
+
+
+def print_separator(width):
+    return '+' + ('---+' * width)
+
+def print_results():
+    for res in result:
+        current = print_separator(res.board_width)
+        current += '\n|'
+        for row in range(res.board_height):
+            for value in range(res.board_width):
+                if res[value][row][u'piece']:
+                    current += ' %s |' % res[value][row][u'piece']().piece_representation()
+                else:
+                    current += '   |'
+            current += '\n'
+            current += print_separator(res.board_width)
+            current += '\n|'
+
+        print current 
+print_results()
